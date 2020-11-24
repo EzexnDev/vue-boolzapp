d0d32488255645abeb7576fbfc09e21f8888afe5
EzexnDev <gianmarco.cavalieredev@gmail.com>
2020-11-24 16:50:29 +0100

    3 e 4 Completati

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var chatList = [{
         user: "Pippo",
         profileimg: "../img/avatar_2.jpg",
+        lastSeen: 'Ultimo accesso oggi alle ' + new Date().toLocaleTimeString(),
         chat: [{
                 text: 'Messaggio di Pippo',
                 sendTime: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
@@ -67,8 +68,8 @@ var chatList = [{
         ]
     },
     {
-        user: "Paperino",
-        profileimg: "../img/avatar_6.jpg",
+        user: "Consuelo",
+        profileimg: "../img/avatar_7.jpg",
         chat: [{
                 text: 'Messaggio di Pippo',
                 sendTime: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
@@ -84,8 +85,8 @@ var chatList = [{
         ]
     },
     {
-        user: "Paperino",
-        profileimg: "../img/avatar_6.jpg",
+        user: "Marina",
+        profileimg: "../img/avatar_8.jpg",
         chat: [{
                 text: 'Messaggio di Pippo',
                 sendTime: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
@@ -101,8 +102,8 @@ var chatList = [{
         ]
     },
     {
-        user: "Paperino",
-        profileimg: "../img/avatar_6.jpg",
+        user: "Jean Claud",
+        profileimg: "../img/avatar_io.jpg",
         chat: [{
                 text: 'Messaggio di Pippo',
                 sendTime: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
@@ -118,8 +119,8 @@ var chatList = [{
         ]
     },
     {
-        user: "Paperino",
-        profileimg: "../img/avatar_6.jpg",
+        user: "Grazia",
+        profileimg: "../img/avatar_2.jpg",
         chat: [{
                 text: 'Messaggio di Pippo',
                 sendTime: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
@@ -135,8 +136,8 @@ var chatList = [{
         ]
     },
     {
-        user: "Paperino",
-        profileimg: "../img/avatar_6.jpg",
+        user: "Genoveffa",
+        profileimg: "../img/avatar_4.jpg",
         chat: [{
                 text: 'Messaggio di Pippo',
                 sendTime: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
@@ -152,8 +153,8 @@ var chatList = [{
         ]
     },
     {
-        user: "Paperino",
-        profileimg: "../img/avatar_6.jpg",
+        user: "Marco",
+        profileimg: "../img/avatar_7.jpg",
         chat: [{
                 text: 'Messaggio di Pippo',
                 sendTime: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
@@ -189,6 +190,7 @@ var app = new Vue({
             return chatArray[chatArray.length - 1].text;
         },
         selectChat(index, chatArray) {
+            chatArray[index].lastSeen = 'Ultimo accesso oggi alle ' + new Date().toLocaleTimeString();
             return chatArray[index];
         },
         setChatIndex(index) {
@@ -205,8 +207,60 @@ var app = new Vue({
                     isViewd: false
                 });
                 this.currentInputMessage = '';
+                this.answerMessage();
             }
             console.log(this.currentInputMessage);
+        },
+
+        answerMessage() {
+            setTimeout(() => {
+                var text = this.chatList[this.chatIndex].chat[this.chatList[this.chatIndex].chat.length - 1].text;
+                var now = new Date();
+                console.log(this.chatList[this.chatIndex].chat[this.chatList[this.chatIndex].chat.length - 1].text);
+                if (text.includes('Ciao') || text.includes('Hey') || text.includes('Yo')) {
+                    this.chatList[this.chatIndex].chat.push({
+                        text: 'Heyyyyyyyyyyyyyyy',
+                        sendTime: now.toLocaleDateString() + ' ' + now.toLocaleTimeString(),
+                        isOwnerMessage: false,
+                        isViewd: false
+                    });
+                } else if (text.includes('Come stai?')) {
+                    this.chatList[this.chatIndex].chat.push({
+                        text: 'Bene e tu?',
+                        sendTime: now.toLocaleDateString() + ' ' + now.toLocaleTimeString(),
+                        isOwnerMessage: false,
+                        isViewd: false
+                    });
+                } else if (text.includes('Bene') || text.includes('Male') || text.includes('Normale')) {
+                    this.chatList[this.chatIndex].chat.push({
+                        text: 'Ho capito.. che fai?',
+                        sendTime: now.toLocaleDateString() + ' ' + now.toLocaleTimeString(),
+                        isOwnerMessage: false,
+                        isViewd: false
+                    });
+                } else if (text.includes('Stavo Programmando')) {
+                    this.chatList[this.chatIndex].chat.push({
+                        text: 'Js?',
+                        sendTime: now.toLocaleDateString() + ' ' + now.toLocaleTimeString(),
+                        isOwnerMessage: false,
+                        isViewd: false
+                    });
+                } else if (text.includes('Coddino?')) {
+                    this.chatList[this.chatIndex].chat.push({
+                        text: 'Warzonataaaaaaaaaaaaaaaaaaa',
+                        sendTime: now.toLocaleDateString() + ' ' + now.toLocaleTimeString(),
+                        isOwnerMessage: false,
+                        isViewd: false
+                    });
+                } else {
+                    this.chatList[this.chatIndex].chat.push({
+                        text: 'Non posso ancora risponderti',
+                        sendTime: now.toLocaleDateString() + ' ' + now.toLocaleTimeString(),
+                        isOwnerMessage: false,
+                        isViewd: false
+                    });
+                }
+            }, Math.floor(Math.random() * (10000 - 600)) + 600);
         },
 
         truncatePreviewMessage(message) {
@@ -222,6 +276,10 @@ var app = new Vue({
             }
             this.filteredChatList = chatList.filter((element) => { return element.user.toLowerCase().includes(filterText.toLowerCase()); });
             console.log(this.filteredChatList);
-        }
+        },
+        scrollToEnd() {
+            var container = this.$el.querySelector("#container");
+            container.scrollTop = container.scrollHeight;
+        },
     }
 });
